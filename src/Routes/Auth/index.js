a7c5d60f10443326b06cdef6c74f00b07cc3da4b
MaEunJa <ttochi0115@gmail.com>
2019-10-19 00:58:10 +0900

    test1019

---
+++ src/Routes/Auth/index.js
@@ -0,0 +1,25 @@
+import  React,{ useState } from "react";
+import styled from "styled-components";
+
+import AuthContainer from "./AuthContainer";
+export default AuthContainer;
+
+/*const Wrapper=styled.div``;
+const Box   =   styled.div`
+    ${props => props.theme.whiteBox}
+`;
+export default()=>{
+    const [action,setAction]    =   useState("logIn");
+
+    //return  action==="logIn" ? "Log in":"Sign Up";
+    return(
+        <Wrapper>
+            {
+                action==="logIn" ?
+                <Box>Log in</Box> : <Box>Sign Up</Box>
+            }
+        </Wrapper>
+    )
+}*/
+
+
